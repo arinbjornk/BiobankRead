1e84340644bcf8936640f6b3e8b7f7544a4c198c
saphir746 <ds711@ic.ac.uk>
2017-06-07 14:41:05 +0100

    new version 2.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except:
 
 setup(name='BiobankRead',
       packages = ['BiobankRead2'],
-      version='2.0',
+      version='2.1',
       description='UKBiobank Python pre-processing',
       author='Deborah Schneider-luftman',
       author_email='ds711@imperial.ac.uk',
